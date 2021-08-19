975f4d453de15a5c600d7083d4fa116114929d22
MartinSkachkov <81876862+MartinSkachkov@users.noreply.github.com>
2021-08-19 17:15:42 +0300

    delete duplicate elements from array

---
+++ Arrays/Del_Duplicate.cs
@@ -0,0 +1,36 @@
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Length: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] arr = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write($"Enter element {i+1}: " );
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i+1; j < length; j++)
+                {
+                    if(arr[i] == arr[j])
+                    {
+                        for (int k = j; k < length-1; k++)
+                        {
+                            arr[k] = arr[k + 1];
+                        }
+                        length--;
+                    }
+                }
+            }
+
+            Console.Write("Elements after insertion:");
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(arr[i] +", ");
+            }
+        }
+    }
